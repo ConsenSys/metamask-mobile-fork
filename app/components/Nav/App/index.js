e81b75c42be2aedcbba8237278ed379b249e7688
Owen Craston <owengc12@gmail.com>
2023-06-09 12:21:40 -0700

    more debugging

--- app/components/Nav/App/index.js
+++ app/components/Nav/App/index.js
@@ -235,6 +235,7 @@ const App = ({ userLoggedIn }) => {
   );
 
   useEffect(() => {
+    console.log('Vault/ NavApp called');
     if (prevNavigator.current || !navigator) return;
     const appTriggeredAuth = async () => {
       const { PreferencesController } = Engine.context;
